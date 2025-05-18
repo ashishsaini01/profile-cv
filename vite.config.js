6b79708819c4c74639f04c4e0948fbbef2436464
Ashish Saini <talktoashish279@gmail.com>
2025-05-18 14:22:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/",
+    base: "profile-cv",
     plugins: [react()],
     build: {
         chunkSizeWarningLimit: 1600,
